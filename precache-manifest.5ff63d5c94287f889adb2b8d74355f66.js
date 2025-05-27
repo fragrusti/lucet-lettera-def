fcc97c46f1036d25f77fcf056fd0c18c67bcb9d9
Francesco Agrusti <francesco@shaBookPro-9.local>
2025-05-27 19:12:53 +0200

    Updates

---
+++ precache-manifest.5ff63d5c94287f889adb2b8d74355f66.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88c73502d1a8d414f789934d8cb90870",
+    "url": "/fragrusti/lucet-lettera-def/index.html"
+  },
+  {
+    "revision": "5af60243a8dc766d90a2",
+    "url": "/fragrusti/lucet-lettera-def/static/css/main.06198879.chunk.css"
+  },
+  {
+    "revision": "7438c0979bdb00ba4e94",
+    "url": "/fragrusti/lucet-lettera-def/static/js/2.383b5087.chunk.js"
+  },
+  {
+    "revision": "2c3509e4d3edb22a793e",
+    "url": "/fragrusti/lucet-lettera-def/static/js/3.5d5b56ad.chunk.js"
+  },
+  {
+    "revision": "f4d81d7c74af68ae277b",
+    "url": "/fragrusti/lucet-lettera-def/static/js/4.073f668c.chunk.js"
+  },
+  {
+    "revision": "5af60243a8dc766d90a2",
+    "url": "/fragrusti/lucet-lettera-def/static/js/main.88d56b94.chunk.js"
+  },
+  {
+    "revision": "5f0df70f8a34b5e71b2f",
+    "url": "/fragrusti/lucet-lettera-def/static/js/runtime~main.d661103d.js"
+  }
+]);
